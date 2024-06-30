2293740f51f0581cf0e7220c292184a021d927d5
Jonathan Faria <jonathanfdsn@gmail.com>
2024-06-30 12:46:11 -0300

    View code

--- avatar_project/avatar_app/views.py
+++ avatar_project/avatar_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import AvatarCharacter
 # Create your views here.
+
+def avatar_character_list(request):
+    characters = AvatarCharacter.objects.all()
+    return render(request, 'template/lista_character.html', {'characters': characters})
