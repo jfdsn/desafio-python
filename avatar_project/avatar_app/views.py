a1dc4851a81b1a890556c58621f57aa4c8fb148c
Jonathan Faria <jonathanfdsn@gmail.com>
2024-07-02 18:49:21 -0300

    Add template style and cache for api data

--- avatar_project/avatar_app/views.py
+++ avatar_project/avatar_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from .api_handler import api_request
 from .translator_handler import translate
 # Create your views here.
 
 def avatar_character_list(request):
-    avatar_api_data = api_request('https://last-airbender-api.fly.dev/api/v1/characters', 'GET')
+    avatar_api_data = cache.get('avatar_api_data')
+    
+    if not avatar_api_data:
+        avatar_api_data = api_request('https://last-airbender-api.fly.dev/api/v1/characters', 'GET')
+        cache.set('avatar_api_data', avatar_api_data, timeout=60*60)
 
     for character in avatar_api_data:
         name = character.get('name')
--- avatar_project/avatar_project/settings.py
+++ avatar_project/avatar_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'avatar_app',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
